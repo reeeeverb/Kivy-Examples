5e6fd0a942745369f4bf8bfe51539a4a5ccc024b
reeeeverb <skydoessoftware@gmail.com>
2023-01-28 19:22:37 -0600

    working board

--- chess-board/buildozer.spec
+++ chess-board/buildozer.spec
@@ -1,7 +1,7 @@
 [app]
 
 # (str) Title of your application
-title = My Application
+title = Chess but awful
 
 # (str) Package name
 package.name = myapp
--- chess-board/main.py
+++ chess-board/main.py
@@ -3,16 +3,24 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.uix.image import Image
+from kivy.core.window import Window
 
 class ChessGame(Widget):
+    s_width = NumericProperty(Window.width)
+    s_height = NumericProperty(Window.height)
     pass
+
+class Chessboard(Widget):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print(touch.pos)
 
 class ChessApp(App):
     def build(self):
         return ChessGame()
 
-class Chessboard(Widget):
-    wimg = Image(source='board.png')
+#class Chessboard(Widget):
+    #wimg = Image(source='board.png')
 
 if __name__ == '__main__':
     ChessApp().run()
