cfc91a8ddb366f4078c72d25b37b750068494499
reeeeverb <skydoessoftware@gmail.com>
2023-01-28 21:40:32 -0600

    added chess pieces

--- chess-board/main.py
+++ chess-board/main.py
@@ -1,3 +1,4 @@
+import math
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty
@@ -11,16 +12,34 @@ class ChessGame(Widget):
     pass
 
 class Chessboard(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.down_square = (0,0)
+
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            print(touch.pos)
+            xpos = touch.pos[0]-self.pos[0]
+            ypos = touch.pos[1]-self.pos[1]
+            self.down_square = self.square_pos(xpos,ypos)
+
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            xpos = touch.pos[0]-self.pos[0]
+            ypos = touch.pos[1]-self.pos[1]
+            up_square = self.square_pos(xpos,ypos)
+            self.legal_move(self.down_square, up_square)
+
+    def square_pos(self,x,y):
+        square_size = self.width/8
+        return (math.trunc(y/square_size)+1,math.trunc(x/square_size)+1)
+
+    def legal_move(self, down, up):
+        if up[0] != 0:
+            print(down, up)
 
 class ChessApp(App):
     def build(self):
         return ChessGame()
-
-#class Chessboard(Widget):
-    #wimg = Image(source='board.png')
 
 if __name__ == '__main__':
     ChessApp().run()
