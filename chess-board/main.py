f158c3581a0de0c03b12a20cb24b98c40cc98a07
reeeeverb <skydoessoftware@gmail.com>
2023-01-30 18:17:32 -0600

    piece setting

--- chess-board/main.py
+++ chess-board/main.py
@@ -1,10 +1,12 @@
 import math
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.uix.image import Image
 from kivy.core.window import Window
+from kivy.lang.builder import Builder
+
 
 class ChessGame(Widget):
     s_width = NumericProperty(Window.width)
@@ -15,12 +17,15 @@ class Chessboard(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.down_square = (0,0)
+    pawn_1 = ObjectProperty(None)
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             xpos = touch.pos[0]-self.pos[0]
             ypos = touch.pos[1]-self.pos[1]
             self.down_square = self.square_pos(xpos,ypos)
+            ## Testing
+            self.parent.pawn_1.set(1,1)
 
     def on_touch_up(self, touch):
         if self.collide_point(*touch.pos):
@@ -40,6 +45,10 @@ class Chessboard(Widget):
 class Pawn(Widget):
     position_row = NumericProperty(5)
     position_col = NumericProperty(6)
+    white = NumericProperty(1)
+    def set(self, row, col):
+        self.position_row = row
+        self.position_col = col
     p_size = NumericProperty(0)
     pass
 
