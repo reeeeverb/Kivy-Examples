44b13f2ddd2f6236eb1d0729a3ec4ebc8e9f74d6
reeeeverb <skydoessoftware@gmail.com>
2023-02-06 18:28:00 -0600

    slightly less elementary piece movement and better piece tracking

--- chess-board/main.py
+++ chess-board/main.py
@@ -28,16 +28,18 @@ class Chessboard(Widget):
         self.marker_present = False
         self.widget_list = []
         self.current_move = "WHITE"
+        self.first = True
 
     def on_touch_down(self, touch):
+        if self.first:
+            self.first = False
+            self.reset_board()
         if self.collide_point(*touch.pos):
             xpos = touch.pos[0]-self.pos[0]
             ypos = touch.pos[1]-self.pos[1]
             self.down_square = self.square_pos(xpos,ypos)
             if self.down_square != self.up_square:
                 self.clear_markers()
-            self.reset_board()
-            print("size ", self.p_size)
             ## Testing
 
     def on_touch_up(self, touch):
@@ -46,11 +48,14 @@ class Chessboard(Widget):
             ypos = touch.pos[1]-self.pos[1]
             self.up_square = self.square_pos(xpos,ypos)
             if self.up_square == self.down_square and self.piece[self.up_square[2] != "EMPTY"]:
+                print("hit")
                 self.generate_moves(self.down_square,True)
             else:
+                self.clear_markers()
                 if self.legal_move(self.down_square, self.up_square):
                     current_piece = (self.names[self.down_square[2]])
                     current_piece.set(self.up_square[0],self.up_square[1])
+                    self.current_move_swap()
 
     def clear_markers(self):
         if self.marker_present:
@@ -88,7 +93,7 @@ class Chessboard(Widget):
                     if show_moves == True:
                         self.show_moves(square[0]+1,square[1]+1)
                     else:
-                        out.append(square+9)
+                        out.append(square[2]+9)
             else:
                 if self.piece[square[2]-8] == "EMPTY":
                     if show_moves == True:
@@ -121,65 +126,56 @@ class Chessboard(Widget):
         self.marker_present=True
 
     def reset_board(self):
+        self.current_move = "WHITE"
         self.parent.w_pawn0.set(1,0)
         self.parent.w_pawn0.makeVisible()
-        self.names[8] = self.parent.w_pawn0
         self.parent.w_pawn1.set(1,1)
         self.parent.w_pawn1.makeVisible()
-        self.names[9] = self.parent.w_pawn1
         self.parent.w_pawn2.set(1,2)
         self.parent.w_pawn2.makeVisible()
-        self.names[10] = self.parent.w_pawn2
         self.parent.w_pawn3.set(1,3)
         self.parent.w_pawn3.makeVisible()
-        self.names[11] = self.parent.w_pawn3
         self.parent.w_pawn4.set(1,4)
         self.parent.w_pawn4.makeVisible()
-        self.names[12] = self.parent.w_pawn4
         self.parent.w_pawn5.set(1,5)
         self.parent.w_pawn5.makeVisible()
-        self.names[13] = self.parent.w_pawn5
         self.parent.w_pawn6.set(1,6)
         self.parent.w_pawn6.makeVisible()
-        self.names[14] = self.parent.w_pawn6
         self.parent.w_pawn7.set(1,7)
         self.parent.w_pawn7.makeVisible()
-        self.names[15] = self.parent.w_pawn7
 
         self.parent.b_pawn0.set(6,0)
         self.parent.b_pawn0.makeVisible()
-        self.names[48] = self.parent.b_pawn0
         self.parent.b_pawn1.set(6,1)
         self.parent.b_pawn1.makeVisible()
-        self.names[49] = self.parent.b_pawn1
         self.parent.b_pawn2.set(6,2)
         self.parent.b_pawn2.makeVisible()
-        self.names[50] = self.parent.b_pawn2
         self.parent.b_pawn3.set(6,3)
         self.parent.b_pawn3.makeVisible()
-        self.names[51] = self.parent.b_pawn3
         self.parent.b_pawn4.set(6,4)
         self.parent.b_pawn4.makeVisible()
-        self.names[52] = self.parent.b_pawn4
         self.parent.b_pawn5.set(6,5)
         self.parent.b_pawn5.makeVisible()
-        self.names[53] = self.parent.b_pawn5
         self.parent.b_pawn6.set(6,6)
         self.parent.b_pawn6.makeVisible()
-        self.names[54] = self.parent.b_pawn6
         self.parent.b_pawn7.set(6,7)
         self.parent.b_pawn7.makeVisible()
-        self.names[55] = self.parent.b_pawn7
 
     def square_pos(self,x,y):
         square_size = self.width/8
         return (math.trunc(y/square_size),math.trunc(x/square_size),math.trunc(y/square_size)*8+math.trunc(x/square_size))
 
     def legal_move(self, down, up):
-        if up[2] in self.generate_moves(down):
+        if self.current_move == self.color[down[2]] and up[2] in self.generate_moves(down):
             return True;
         else:
             return False;
+
+    def current_move_swap(self):
+        if self.current_move == "WHITE":
+            self.current_move = "BLACK"
+        elif self.current_move == "BLACK":
+            self.current_move = "WHITE"
 
 class Pawn(Widget):
     position_row = NumericProperty(0)
@@ -197,8 +193,15 @@ class Pawn(Widget):
         self.parent.piece[row*8+col] = "PAWN"
 
     def set(self, row, col):
+        self.parent.names[self.position_row*8+self.position_col] = "EMPTY"
+        self.parent.piece[self.position_row*8+self.position_col] = "EMPTY"
+        self.parent.color[self.position_row*8+self.position_col] = "EMPTY"
+
         self.position_col = col
         self.position_row = row
+        self.parent.names[self.position_row*8+self.position_col] = self
+        self.parent.piece[self.position_row*8+self.position_col] = "PAWN"
+        self.parent.color[self.position_row*8+self.position_col] = "WHITE" if self.white else "BLACK"
         if self.visible == 1:
             self.parent.color[row*8+col] = "WHITE" if self.white == 1 else "BLACK"
             self.parent.piece[row*8+col] = "PAWN"
